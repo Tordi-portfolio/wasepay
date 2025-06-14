4ab6ca21cc56677c0c70ce551ea2ab1c48cdbf71
Tordi-portfolio <164408531+Tordi-portfolio@users.noreply.github.com>
2025-06-14 11:51:44 +0100

    Delete invest directory

--- invest/project/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Plan, Wallet, WithdrawalRequest, Transaction
-# Register your models here.
-
-admin.site.register(Plan)
-admin.site.register(Wallet)
-admin.site.register(Transaction)
-admin.site.register(WithdrawalRequest)
